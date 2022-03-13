c26ea2f803b9cf0c2e1aaf59ef5797c2ce7e573b
Drumus <miguelibanezpico@gmail.com>
2022-03-13 19:42:45 +0100

    Button clear propts the user for the size of the grid and builds it

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const body = document.querySelector('body');
+/*const body = document.querySelector('body');
 const divContainer = document.createElement('div');
 divContainer.setAttribute('id', 'container');
 
@@ -16,14 +16,11 @@ for (let i = 0; i < 4; i++)
     divContainer.appendChild(row);
 }
 body.appendChild(divContainer);
+*/
+makeGrid(4);
 
 //hover
-const contentAll = document.querySelectorAll('.content');
-contentAll.forEach(contentAll => {
-    contentAll.addEventListener('mouseover', (e) => {
-        contentAll.setAttribute('style', 'background-color: blue');
-    });
-});
+
 
 
 function removeGrid() {
@@ -31,4 +28,34 @@ function removeGrid() {
     allDivs.forEach(allDivs => {
         allDivs.remove();
     });
+    let gridSize = window.prompt('Grid size: ');
+    makeGrid(gridSize);
+}
+
+function makeGrid(gridSize) {
+    const body = document.querySelector('body');
+    const divContainer = document.createElement('div');
+    divContainer.setAttribute('id', 'container');
+
+    for (let i = 0; i < gridSize; i++)
+    {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+
+        for (let i = 0; i < gridSize; i++)
+        {
+            let content = document.createElement('div');
+            content.setAttribute('class', 'content');
+            row.appendChild(content);
+        }
+        divContainer.appendChild(row);
+    }
+    body.appendChild(divContainer);
+    //hover
+    const contentAll = document.querySelectorAll('.content');
+    contentAll.forEach(contentAll => {
+    contentAll.addEventListener('mouseover', (e) => {
+        contentAll.setAttribute('style', 'background-color: blue');
+    });
+});
 }
